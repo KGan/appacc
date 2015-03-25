b4690e3ac593cbb77ada75a5c6b1d605c7fe252b
App Academy Student <student@appacademy.io>
2015-03-24 21:54:59 -0700

    lols

---
+++ assessprac.js
@@ -0,0 +1,66 @@
+'use strict';
+
+
+Function.prototype.myBind = function(context) {
+  var fn = this
+  var args = Array.prototype.slice.call(arguments, 1)
+  return function() {
+    var calltime_args = Array.prototype.slice.call(arguments)
+    fn.apply(context, Array.prototype.concat.call(args, calltime_args))
+  }
+}
+
+
+function myinherits(child, parent) {
+  function Surrogate(){};
+  Surrogate.prototype = parent.prototype;
+  child.prototype = new Surrogate();
+}
+
+Function.prototype.mycurry = function(numArgs) {
+  var fn = this;
+  var _args = Array.prototype.slice.call(arguments,1)
+  if (_args.length >= numArgs) {
+    return fn.apply(null, _args);
+  }
+
+  return function _curry(arg) {
+    _args.push(arg)
+    if(_args.length >= numArgs) {
+      return fn.apply(null, _args);
+    } else {
+      return _curry;
+    }
+
+
+  }
+}
+
+function sum() {
+  var sum = 0;
+  for(var i = 0; i < arguments.length; i++) {
+    sum += arguments[i];
+  }
+  return sum;
+}
+
+// console.log(sum.mycurry(4)(1)(3)(2)(10))
+
+function bsearch(arr, target) {
+  var idx = (arr.length / 2 | 0);
+  var saved_idx = Array.prototype.slice.call(arguments,2)[0] || 0
+  if(arr.length < 2) {
+    return arr[0] == target ? saved_idx : -1
+  }
+
+  if(arr[idx] == target) {
+    return saved_idx + idx;
+  }
+
+  if(arr[idx] < target) {
+    return bsearch(arr.slice(0,idx), target);
+  }
+  return bsearch(arr.slice(idx+1), target, saved_idx + idx + 1)
+}
+
+console.log(bsearch([1,3,5,12,18,22,345], 12))
---
+++ dp.js
@@ -0,0 +1,78 @@
+'use strict'
+
+function printmatrix(m) {
+  m.forEach(function(row, idx) {
+    var rowstr = "";
+    row.forEach(function(el, jdx) {
+      var cand = el;
+      if(typeof el === 'boolean'){
+        cand = el ? 'T' : 'F';
+      }
+      rowstr += cand + '  ';
+    })
+    console.log(rowstr)
+  })
+}
+
+function editdistance(first, second) {
+  var mat = new Array(first.length + 1);
+  for(var i = 0; i < mat.length; i++) {
+    mat[i] = new Array(second.length + 1);
+  }
+  for(i = 0; i < mat.length; i++) {
+    mat[i][0] = i;
+  }
+  for(i = 0; i < mat[0].length; i++) {
+    mat[0][i] = i;
+  }
+
+  for(var j = 1; j < mat.length; j++) {
+    for(var k = 1; k < mat[0].length; k++) {
+      if(first[j-1] == second[k-1]) {
+        mat[j][k] = mat[j-1][k-1]
+      } else {
+        mat[j][k] = Math.min.apply(null, [mat[j-1][k-1], mat[j-1][k], mat[j][k-1]]) + 1
+      }
+    }
+  }
+
+
+
+  printmatrix(mat)
+  return mat[first.length][second.length];
+}
+
+// console.log(editdistance('chips', 'chocolate'))
+
+function chocodistance(target, first, second) {
+  var mat = new Array(first.length + 1);
+  for(var i = 0; i < mat.length; i++) {
+    mat[i] = new Array(second.length + 1);
+  }
+
+  target = " " + target
+
+  mat[0][0] = true;
+
+  for(i = 0; i < mat.length; i++) {
+    for(var j = 0; j < mat[0].length; j++) {
+      if (i == 0 && j == 0) {
+        continue;
+      }
+      if(target[i+j] === first[i - 1] || target[i+j] === second[j - 1]) {
+        mat[i][j] = (!!(mat[i-1] && mat[i-1][j] ) || !!mat[i][j-1]);
+      } else {
+        mat[i][j] = false;
+      }
+    }
+  }
+
+  printmatrix(mat);
+
+  return mat[first.length][second.length];
+}
+
+
+
+// console.log(chocodistance('chocolatechips', 'chocolate', 'chips'));
+console.log(chocodistance('cchohicopslate', 'chocolate', 'chips'))
